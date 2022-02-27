38fc5f742495f0fe0bd5bf5ad8c3a07bbfbc4e4a
Stephen Ulibarri <druidmech@gmail.com>
2022-02-26 18:03:45 -0700

    Drawing the Crosshairs

--- Source/Blaster/HUD/BlasterHUD.cpp
+++ Source/Blaster/HUD/BlasterHUD.cpp
@@ -7,5 +7,54 @@ void ABlasterHUD::DrawHUD()
 {
 	Super::DrawHUD();
 
+	FVector2D ViewportSize;
+	if (GEngine)
+	{
+		GEngine->GameViewport->GetViewportSize(ViewportSize);
+		const FVector2D ViewportCenter(ViewportSize.X / 2.f, ViewportSize.Y / 2.f);
 
+		if (HUDPackage.CrosshairsCenter)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsCenter, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsLeft)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsLeft, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsRight)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsRight, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsTop)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsTop, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsBottom)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsBottom, ViewportCenter);
+		}
+	}
+}
+
+void ABlasterHUD::DrawCrosshair(UTexture2D* Texture, FVector2D ViewportCenter)
+{
+	const float TextureWidth = Texture->GetSizeX();
+	const float TextureHeight = Texture->GetSizeY();
+	const FVector2D TextureDrawPoint(
+		ViewportCenter.X - (TextureWidth / 2.f),
+		ViewportCenter.Y - (TextureHeight / 2.f)
+	);
+
+	DrawTexture(
+		Texture,
+		TextureDrawPoint.X,
+		TextureDrawPoint.Y,
+		TextureWidth,
+		TextureHeight,
+		0.f,
+		0.f,
+		1.f,
+		1.f,
+		FLinearColor::White
+	);
 }
--- Source/Blaster/HUD/BlasterHUD.h
+++ Source/Blaster/HUD/BlasterHUD.h
@@ -29,6 +29,8 @@ public:
 	virtual void DrawHUD() override;
 private:
 	FHUDPackage HUDPackage;
+
+	void DrawCrosshair(UTexture2D* Texture, FVector2D ViewportCenter);
 public:
 	FORCEINLINE void SetHUDPackage(const FHUDPackage& Package) { HUDPackage = Package; }
 };
